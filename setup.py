b313b2185f481cac4ddb3fdf8684bb485bc01fef
Niraj Adhikari <nrjadkry@gmail.com>
2024-02-02 14:54:29 +0545

    setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='nepali-date-utils',
+    version='0.1',
+    description='Convert Nepali to English Dates: Easily switch between Nepali and English dates with this Python package.',
+    author='Niraj Adhikari',
+    author_email='nrjadkry@gmail.com',
+    packages=find_packages(),
+    package_data={'nepali-date-utils': ['nepali-date-utils/docs/user_guide.md']},
+    license='GPLv3',
+    url='https://github.com/nrjadkry/nepali-date-utils',
+    install_requires=[],
+    keywords=['date','nepali-date'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
